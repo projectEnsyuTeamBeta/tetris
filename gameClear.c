fab859e263a1a8051629b64561e36de528af9b84
OkumuraTomoki <bass.sound.105@gmail.com>
2024-07-26 08:45:29 +0900

    main関数作りました

---
+++ gameClear.c
@@ -0,0 +1,18 @@
+
+
+#include "lcdlib.h"
+#include "i2clib.h"
+#include "ad.h"
+
+
+void main(void)
+{
+	initBase();
+	initI2C();
+	initLCD();
+	initAD();
+
+	while (1) {
+		printFstr(5,5,"Game Clear!!");
+	}
+}
