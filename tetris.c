c6f0e48ca2987ddc30d46e7894fc264f96b498e1
OkumuraTomoki <bass.sound.105@gmail.com>
2024-07-26 09:47:19 +0900

    main関数修正

--- tetris.c
+++ tetris.c
@@ -1,15 +1,28 @@
 #include "lcdlib.h"
 #include "i2clib.h"
-#include "ad.h"
+
+#define WALL_HEIGHT     20
+#define FLOOR_WIDTH     10 + 2
 
 void main(void)
 {
+    int i;
+    int j;
 
     initBase();
 	initI2C();
 	initLCD();
 
+
 	while (1) {
+        for(i = 5; i < FLOOR_WIDTH; i++){
+            drawString(i,0,"?");
+        }
+        for(j = 0; j < WALL_HEIGHT; j++){
+            drawString(5,j,"?");
+            drawString(17,j,"?");
+        }
+        
         PORT0.PDR.BIT.B4 = 0;
         PORT0.PDR.BIT.B5 = 0;
         PORT0.PDR.BIT.B6 = 0;
@@ -20,5 +33,4 @@ void main(void)
             gameOver();
         }
 	}
-    
 }
