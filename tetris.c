9ff0de4bad7a2f88fddcbd77d878d797386c8727
hmmtr25 <syjnkusuzm.mski@gmail.com>
2024-07-26 14:49:12 +0900

    a

--- tetris.c
+++ tetris.c
@@ -10,9 +10,13 @@ void gameClear(void);
 void gameOver(void);
 void main(void)
 {
+	char I[4]={"####"};
     int i, j;
     int k, l;
-    int stage = 1; //stage関数で0or1
+    int x,y;
+    int stage = 0; //stage関数で0or1
+    x=(HAICHI+5);
+    
 
     initBase();
 	initI2C();
@@ -28,7 +32,7 @@ void main(void)
             printFstr(HAICHI,j,"?");
             printFstr(HAICHI + 11,j,"?");
         }
-        for(k = (HAICHI+1); k < (HAICHI+11);k++){
+       /* for(k = (HAICHI+1); k < (HAICHI+11);k++){
             for(l = 1; l < (WALL_HEIGHT + 1); l++){
                 if(stage == 0){
                     printFstr(k,l," ");     // Aを変更（0：非表示/1：ミノ配置）
@@ -36,7 +40,15 @@ void main(void)
                     printFstr(k,l,"A");
                 }
             }
-        }
+        }*/
+	
+	
+	/*for(y=5; y<9; y++){
+		printFstr(x,y,"I");
+	}*/
+	printFstr(x,18,"%s",I);
+	
+	
         
         PORT0.PDR.BIT.B0 = 0;
         PORT0.PDR.BIT.B1 = 0;
